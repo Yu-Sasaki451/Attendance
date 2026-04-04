8522f14835f45b00fed901b9f0b5f31dd664b150
Yu Sasaki <uyu17018@gmail.com>
2026-04-04 16:32:51 +0900

    css修正

--- src/resources/views/admin/correction_request_detail.blade.php
+++ src/resources/views/admin/correction_request_detail.blade.php
@@ -17,18 +17,18 @@
     <h1 class="page-title">勤怠詳細</h1>
 
     <table class="attendance-table">
-        <tr class="table-row">
-            <th class="name-col">名前</th>
-            <td class="name-col">
-                <div class="detail-inline-row">
+        <tr class="table-row table-row--name">
+            <th class="name-row">名前</th>
+            <td class="name-row">
+                <div class="detail-row">
                     <span class="detail-name-text">{{ $userName }}</span>
                 </div>
             </td>
         </tr>
         <tr class="table-row">
-            <th class="date-col">日付</th>
-            <td class="date-col">
-                <div class="detail-inline-row">
+            <th class="date-row">日付</th>
+            <td class="date-row">
+                <div class="detail-row">
                     <span class="detail-date-text">{{ $dateYearLabel }}</span>
                     <span></span>
                     <span class="detail-date-text">{{ $dateMonthDayLabel }}</span>
@@ -36,13 +36,13 @@
             </td>
         </tr>
         <tr class="table-row">
-            <th class="in-col">出勤・退勤</th>
-            <td class="in-col">
+            <th class="in-row">出勤・退勤</th>
+            <td class="in-row">
                 <div class="detail-field">
-                    <div class="detail-inline-row">
+                    <div class="detail-row">
                         <input class="detail-input" type="time" name="in_at" value="{{ $inAtLabel }}">
                         <span class="detail-time-separator">〜</span>
-                        <input class="detail-input detail-input" type="time" name="out_at" value="{{ $outAtLabel }}">
+                        <input class="detail-input" type="time" name="out_at" value="{{ $outAtLabel }}">
                     </div>
                     <div class="validate-error">
                         @error('attendance_time')
@@ -54,10 +54,10 @@
         </tr>
         @foreach($breakRows as $breakRow)
         <tr class="table-row js-break-row">
-            <th class="break-col">{{ $breakRow['label'] }}</th>
-            <td class="break-col">
+            <th class="break-row">{{ $breakRow['label'] }}</th>
+            <td class="break-row">
                 <div class="detail-field">
-                    <div class="detail-inline-row">
+                    <div class="detail-row">
                         <input class="detail-input js-break-in" type="time" name="break_in_at[]" value="{{ $breakRow['in_at'] }}">
                         <span class="detail-time-separator">〜</span>
                         <input class="detail-input js-break-out" type="time" name="break_out_at[]" value="{{ $breakRow['out_at'] }}">
@@ -71,9 +71,9 @@
             </td>
         </tr>
         @endforeach
-        <tr class="table-row--note js-note-row">
-            <th class="note-col">備考</th>
-            <td class="note-col">
+        <tr class="table-row table-row--note js-note-row">
+            <th class="note-row">備考</th>
+            <td class="note-row">
                 <div class="detail-field">
                     <textarea class="detail-textarea" name="note">{{ $noteLabel }}</textarea>
                     <div class="validate-error">
@@ -88,9 +88,9 @@
 
     <div class="approved-item">
     @if($isPending)
-        <button class="detail-button" type="submit">承認</button>
+        <button class="approve-button" type="submit">承認</button>
     @else
-        <button class="detail-button">承認済み</button>
+        <button class="button-approved">承認済み</button>
     @endif
     </div>
 
